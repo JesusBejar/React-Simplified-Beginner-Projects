e3306966bc5611a4fa8d9fcb70cc72d4c01e9388
Jesus Bejar <israelbejarii@gmail.com>
2025-03-17 07:36:20 -0500

    rendered userCard and userCardClass

--- 15-16-user-card/before/src/App.jsx
+++ 15-16-user-card/before/src/App.jsx
@@ -1,7 +1,27 @@
 import { useState } from "react";
 import styles from "./user.css";
 import json from "./user.json";
+import { UserCard } from "./Usercard";
+import { UserCardClass } from "./UserCardClass";
 
-function App() {}
+export default function App() {
+  return (
+    <div>
+      <UserCard
+        name={user.name}
+        number={user.number}
+        age={user.age}
+        address={user.address}
+      />
 
-export default App;
+      <h1></h1>
+
+      <UserCardClass>
+        name={user.name}
+        number={user.number}
+        age={user.age}
+        address={user.address}
+      </UserCardClass>
+    </div>
+  );
+}

74e9eaab0d481dc319b3ebb7f8644f663e57234f
Jesus Bejar <israelbejarii@gmail.com>
2025-03-20 14:20:33 -0600

    added ability to remove first element

--- 20-array-component/src/App.jsx
+++ 20-array-component/src/App.jsx
@@ -3,9 +3,24 @@ import { useState } from "react";
 const INITIAL_VALUE = ["A", "B", "C"];
 
 function App() {
+  const [array, setArray] = useState(INITIAL_VALUE);
   const [value, setValue] = useState("");
 
-  return "";
+  // slice and dice bb
+  function removeFirstElement() {
+    setArray((currentArray) => {
+      return currentArray.slice(1);
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={removeFirstElement}>Remove first elemment</button>
+      <br />
+
+      {array.join(", ")}
+    </div>
+  );
 }
 
 export default App;

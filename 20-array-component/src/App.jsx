d87810f050be3a39cf386387aeec5814d16a9dec
Jesus Bejar <israelbejarii@gmail.com>
2025-03-20 18:46:06 -0600

    added ability to remove letter, add letter to start and end

--- 20-array-component/src/App.jsx
+++ 20-array-component/src/App.jsx
@@ -13,10 +13,33 @@ function App() {
     });
   }
 
+  function removeSpecificLetter(letter) {
+    setArray((currentArray) => {
+      return currentArray.filter((element) => element !== letter);
+    });
+  }
+
+  function addLetterToStart(letter) {
+    setArray((currentArray) => {
+      return [letter, ...currentArray];
+    });
+  }
+
+  function addLetterToEnd(letter) {
+    setArray((currentArray) => {
+      return [...currentArray, letter];
+    });
+  }
+
   return (
     <div>
       <button onClick={removeFirstElement}>Remove first elemment</button>
       <br />
+      <button onClick={() => removeSpecificLetter("C")}>Remove all C's</button>
+      <br />
+      <button onClick={() => addLetterToStart("B")}>Add B to beginning</button>
+      <br />
+      <button onClick={() => addLetterToEnd("A")}>Add A to end</button>
 
       {array.join(", ")}
     </div>

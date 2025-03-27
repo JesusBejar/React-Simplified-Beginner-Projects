3179b96870710d5db05720505aa8f554e34f7316
Jesus Bejar <israelbejarii@gmail.com>
2025-03-27 14:39:26 -0600

    first useEffect exercises

--- 27-28-use-effect-exercises/before/src/App.jsx
+++ 27-28-use-effect-exercises/before/src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react"
-import { Child } from "./Child"
+import { useState } from "react";
+import { Child } from "./Child";
 
 export default function App() {
-  const [show, setShow] = useState(true)
+  const [show, setShow] = useState(true);
 
-  const childComponent = show ? <Child /> : null
+  useEffect(() => {
+    console.log("Hi - mounted");
+  }, [show]);
+
+  const childComponent = show ? <Child /> : null;
 
   return (
     <div>
-      <button onClick={() => setShow(currentShow => !currentShow)}>
+      <button onClick={() => setShow((currentShow) => !currentShow)}>
         Show/Hide
       </button>
       {childComponent}
     </div>
-  )
+  );
 }
